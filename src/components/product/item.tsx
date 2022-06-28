8b11a9841e3e1b05493d3e99b6fb0d9acb667204
crewd <newspaper321@nate.com>
2022-06-28 17:40:21 +0900

    recoil 추가

--- src/components/product/item.tsx
+++ src/components/product/item.tsx
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom'
+import { useRecoilState, useRecoilValue } from 'recoil';
 import { Product } from '../../graphql/products';
+import { cartItemSelector } from '../../recoils/cart';
 
 const Productitem = ({
   description,
@@ -8,14 +10,23 @@ const Productitem = ({
   price,
   title,
   createdAt
-}: Product) => (
-  <li className="product-item">
-    <Link to={`/products/${id}`}>
-      <p className="product-item__title">{title}</p>
-      <img className="product-item__image" src={imageUrl} />
-      <span>${price}</span>
-    </Link>
-  </li>
-)
+}: Product) => {
+  const [cartAmount, setCartAmount] = useRecoilState(cartItemSelector(id));
+
+  const addToCart = () => setCartAmount((cartAmount || 0) + 1);
+
+  return (
+    <li className="product-item">
+      <Link to={`/products/${id}`}>
+        <p className="product-item__title">{title}</p>
+        <img className="product-item__image" src={imageUrl} />
+        <span>${price}</span>
+      </Link>
+      <button className="product-item__add-cart" onClick={addToCart}>담기</button>
+      <span>{cartAmount || 0}</span>
+    </li>
+  )
+
+}
 
 export default Productitem;
