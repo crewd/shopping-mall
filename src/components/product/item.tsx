8c2a0d98c44e9195ede7aea6523bd2d021b5ba5b
crewd <newspaper321@nate.com>
2022-06-28 20:07:35 +0900

    장바구니 추가

--- src/components/product/item.tsx
+++ src/components/product/item.tsx
@@ -1,7 +1,8 @@
+import { useMutation } from 'react-query';
 import { Link } from 'react-router-dom'
-import { useRecoilState, useRecoilValue } from 'recoil';
+import { ADD_CART } from '../../graphql/cart';
 import { Product } from '../../graphql/products';
-import { cartItemSelector } from '../../recoils/cart';
+import { graphqlFetcher } from '../../queryClient';
 
 const Productitem = ({
   description,
@@ -11,9 +12,7 @@ const Productitem = ({
   title,
   createdAt
 }: Product) => {
-  const [cartAmount, setCartAmount] = useRecoilState(cartItemSelector(id));
-
-  const addToCart = () => setCartAmount((cartAmount || 0) + 1);
+  const { mutate: addCart } = useMutation((id: string) => graphqlFetcher(ADD_CART, { id }))
 
   return (
     <li className="product-item">
@@ -22,8 +21,7 @@ const Productitem = ({
         <img className="product-item__image" src={imageUrl} />
         <span>${price}</span>
       </Link>
-      <button className="product-item__add-cart" onClick={addToCart}>담기</button>
-      <span>{cartAmount || 0}</span>
+      <button className="product-item__add-cart" onClick={() => addCart(id)}>담기</button>
     </li>
   )
 
