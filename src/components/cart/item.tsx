dfe7a1fca30e9232e5a64d5d6902cd3243bb2e26
crewd <newspaper321@nate.com>
2022-06-30 18:48:11 +0900

    장바구니 삭제

--- src/components/cart/item.tsx
+++ src/components/cart/item.tsx
@@ -1,11 +1,10 @@
 import { SyntheticEvent } from 'react'
 import { useMutation } from "react-query";
-import { CartType, UPDATE_CART } from "../../graphql/cart";
+import { CartType, DELETE_CART, UPDATE_CART } from "../../graphql/cart";
 import { getClient, graphqlFetcher, QueryKeys } from "../../queryClient";
 
 
 const CartItem = ({ id, imageUrl, price, title, amount }: CartType) => {
-
 
   const queryClient = getClient();
 
@@ -13,26 +12,41 @@ const CartItem = ({ id, imageUrl, price, title, amount }: CartType) => {
     ({ id, amount }: { id: string; amount: number }) => graphqlFetcher(UPDATE_CART, { id, amount }),
     {
       onMutate: async ({ id, amount }) => {
-        await queryClient.cancelQueries(QueryKeys.CART)
-        const prevCart = queryClient.getQueryData<{ [key: string]: CartType }>(QueryKeys.CART)
-        if (!prevCart?.[id]) return prevCart
+        await queryClient.cancelQueries(QueryKeys.CART);
+        const prevCart = queryClient.getQueryData<{ [key: string]: CartType }>(QueryKeys.CART);
+
+        if (!prevCart?.[id]) {
+          return prevCart;
+        }
 
         const newCart = {
           ...(prevCart || {}),
           [id]: { ...prevCart[id], amount },
-        }
-        queryClient.setQueryData(QueryKeys.CART, newCart)
-        return prevCart
+        };
+
+        queryClient.setQueryData(QueryKeys.CART, newCart);
+        return prevCart;
       },
       onSuccess: newValue => {
-        const prevCart = queryClient.getQueryData<{ [key: string]: CartType }>(QueryKeys.CART)
+        const prevCart = queryClient.getQueryData<{ [key: string]: CartType }>(QueryKeys.CART);
         const newCart = {
           ...(prevCart || {}),
           [id]: newValue,
-        }
+        };
+
         queryClient.setQueryData(QueryKeys.CART, newCart)
       },
     },
+  )
+
+  const { mutate: deleteCart } = useMutation(
+    ({ id }: { id: string }) => graphqlFetcher(DELETE_CART, { id }),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(QueryKeys.CART)
+      }
+    }
+
   )
 
   const handleUpdateAmount = (e: SyntheticEvent) => {
@@ -40,8 +54,13 @@ const CartItem = ({ id, imageUrl, price, title, amount }: CartType) => {
     updateCart({ id, amount })
   }
 
+  const handleDeleteItem = () => {
+    deleteCart({ id })
+  }
+
   return (
     <li className="cart-item">
+      <input className='cart-item__checkbox' type="checkbox" />
       <img className="cart-item__image" src={imageUrl} />
       <p className="cart-item__price">{price}</p>
       <p className="cart-item__title">{title}</p>
@@ -51,6 +70,7 @@ const CartItem = ({ id, imageUrl, price, title, amount }: CartType) => {
         value={amount}
         onChange={handleUpdateAmount}
       />
+      <button className='cart-item__button' onClick={handleDeleteItem}>삭제</button>
     </li>
   )
 
